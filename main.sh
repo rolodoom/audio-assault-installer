61997d6bb4cff59b62bf3d8ade77e4e083bf0148
rolodoom <info@rolandoramostorres.com>
2021-01-09 10:54:24 -0500

    Update

--- main.sh
+++ main.sh
@@ -34,6 +34,8 @@ aa_install () {
         unzip -q 'Duality Bass Studio 1.2.51 Installer.zip' -d "$TEMPDIR"/Duality
         unzip -q 'Sigma v2 upd103.zip' -d "$TEMPDIR"/Sigma
         unzip -q 'RVXX v2 upd103.zip' -d "$TEMPDIR"/RVXX
+        unzip -q 'Sigma 102.zip' -d "$TEMPDIR"/Sigma
+        unzip -q 'RVXX Installer 1.0.1.zip' -d "$TEMPDIR"/RVXX
     fi
 
     # clean mac Files from installers
@@ -59,27 +61,38 @@ aa_install () {
     fi
 
     # copy Stand Alone
-    sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality" "$AUDIOASSAULTBIN"
-    sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality Bass Studio" "$AUDIOASSAULTBIN/Duality"
+    #v2
     sudo cp -r "$TEMPDIR/RVXX/RVXX v2/RVXX v2 Linux/RVXX" "$AUDIOASSAULTBIN"
     sudo cp -r "$TEMPDIR/RVXX/RVXX v2/RVXX v2 Linux/RVXX v2 Standalone" "$AUDIOASSAULTBIN/RVXX"
     sudo cp -r "$TEMPDIR/Sigma/Sigma v2/Sigma v2 Linux/Sigma" "$AUDIOASSAULTBIN"
     sudo cp -r "$TEMPDIR/Sigma/Sigma v2/Sigma v2 Linux/Sigma v2 Standalone" "$AUDIOASSAULTBIN/Sigma"
-    
+    # v1
+    sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality" "$AUDIOASSAULTBIN"
+    sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality Bass Studio" "$AUDIOASSAULTBIN/Duality"
+    sudo cp -r "$TEMPDIR/RVXX/RVXX Installer 1.0.1/RVXX Linux /RVXX STANDALONE 101" "$AUDIOASSAULTBIN/RVXX"
+    sudo cp -r "$TEMPDIR/Sigma/Sigma Standalone 102" "$AUDIOASSAULTBIN/Sigma"
+
     #chmod 777
     sudo chmod -R 777 "$AUDIOASSAULTBIN/Duality/"
     sudo chmod -R 777 "$AUDIOASSAULTBIN/RVXX/"
     sudo chmod -R 777 "$AUDIOASSAULTBIN/Sigma/"
 
     # chmod
-    sudo chmod +x "$AUDIOASSAULTBIN/Duality/Duality Bass Studio"
+    #v2
     sudo chmod +x "$AUDIOASSAULTBIN/RVXX/RVXX v2 Standalone"
     sudo chmod +x "$AUDIOASSAULTBIN/Sigma/Sigma v2 Standalone"
+    #v1
+    sudo chmod +x "$AUDIOASSAULTBIN/Duality/Duality Bass Studio"
+    sudo chmod +x "$AUDIOASSAULTBIN/RVXX/RVXX STANDALONE 101"
+    sudo chmod +x "$AUDIOASSAULTBIN/Sigma/Sigma Standalone 102"
 
     # usr/local/bin links
-    sudo ln -s "$AUDIOASSAULTBIN/Duality/Duality Bass Studio" "/usr/local/bin/duality-bass-studio"
-    sudo ln -s "$AUDIOASSAULTBIN/RVXX/RVXX v2 Standalone" "/usr/local/bin/rvxx"
-    sudo ln -s "$AUDIOASSAULTBIN/Sigma/Sigma v2 Standalone" "/usr/local/bin/sigma"
+    sudo ln -s "$AUDIOASSAULTBIN/RVXX/RVXX v2 Standalone" "/usr/local/bin/rvxx_v2"
+    sudo ln -s "$AUDIOASSAULTBIN/Sigma/Sigma v2 Standalone" "/usr/local/bin/sigma_v2"
+    #v1
+    sudo ln -s "$AUDIOASSAULTBIN/Duality/Duality Bass Studio" "/usr/local/bin/duality-bass-studio_v1"
+    sudo ln -s "$AUDIOASSAULTBIN/RVXX/RVXX STANDALONE 101" "/usr/local/bin/rvxx_v1"
+    sudo ln -s "$AUDIOASSAULTBIN/Sigma/Sigma v2 Standalone" "/usr/local/bin/sigma_v1"
 
     # copy .local files
     sudo cp -r usr/share /usr
@@ -97,22 +110,24 @@ aa_uninstall () {
     # @temp
     sudo rm -rf "$TEMPDIR"
     # desktop files
-    sudo rm -rf /usr/share/applications/sigma.desktop
-    sudo rm -rf /usr/share/applications/rvxx.desktop
-    sudo rm -rf /usr/share/applications/duality-bass-studio.desktop
+    sudo rm -rf /usr/share/applications/sigma_v2.desktop
+    sudo rm -rf /usr/share/applications/rvxx_v2.desktop
+    sudo rm -rf /usr/share/applications/duality-bass-studio_v1.desktop
+    sudo rm -rf /usr/share/applications/rvxx_v1.desktop
+    sudo rm -rf /usr/share/applications/sigma_v1.desktop
     # icons
-    sudo rm -rf /usr/share/icons/hicolor/48x48/apps/sigma.png
-    sudo rm -rf /usr/share/icons/hicolor/48x48/apps/rvxx.png
-    sudo rm -rf /usr/share/icons/hicolor/48x48/apps/duality-bass-studio.png
-    sudo rm -rf /usr/share/icons/hicolor/256x256/apps/sigma.png
-    sudo rm -rf /usr/share/icons/hicolor/256x256/apps/duality-bass-studio.png
+    sudo rm -rf /usr/share/icons/sigma.png
+    sudo rm -rf /usr/share/icons/rvxx.png
+    sudo rm -rf /usr/share/icons/duality-bass-studio.png
     # vst
     sudo rm -rf "$AUDIOASSAULT"
     # bin
     sudo rm -rf "$AUDIOASSAULTBIN"
-    sudo rm -rf /usr/local/bin/duality-bass-studio
-    sudo rm -rf /usr/local/bin/rvxx
-    sudo rm -rf /usr/local/bin/sigma
+    sudo rm -rf /usr/local/bin/rvxx_v2
+    sudo rm -rf /usr/local/bin/sigma_v2
+    sudo rm -rf /usr/local/bin/duality-bass-studio_v1
+    sudo rm -rf /usr/local/bin/rvxx_v1
+    sudo rm -rf /usr/local/bin/sigma_v1
 
     echo "Done!!!"
 }
