78228e3790c4a1c8fa805237be928c77621035f8
rolodoom <rolodoom@gmail.com>
2021-11-10 11:53:43 -0500

    BlackStar added

--- main.sh
+++ main.sh
@@ -36,8 +36,10 @@ aa_install () {
         unzip -q 'RVXX v2 upd103.zip' -d "$TEMPDIR"/RVXX
         #v1
         unzip -q 'Duality Bass Studio 1.2.51 Installer.zip' -d "$TEMPDIR"/Duality
-        unzip -q 'Sigma 102.zip' -d "$TEMPDIR"/Sigma
         unzip -q 'RVXX Installer 1.0.1.zip' -d "$TEMPDIR"/RVXX
+        unzip -q 'Sigma 102.zip' -d "$TEMPDIR"/Sigma
+        unzip -q 'Blacksun+Installers+v1.zip' -d "$TEMPDIR"/BlackSun
+        
     fi
 
     # clean mac Files from installers
@@ -57,9 +59,9 @@ aa_install () {
     sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality Bass Studio.so" "$AUDIOASSAULT"
     sudo cp -r "$TEMPDIR/Sigma/SigmaVST 102.so" "$AUDIOASSAULT"
     sudo cp -r "$TEMPDIR/RVXX/RVXX Installer 1.0.1/RVXX Linux /RVXX VST 101.so" "$AUDIOASSAULT"
-    
+    sudo cp -r "$TEMPDIR/BlackSun/Blacksun Linux/Blacksunvst2.so" "$AUDIOASSAULT"
 
-    # check if $HOME/.audio-assault Assault exists
+    # check if /opt/Audio exists
     if [ ! -d "$AUDIOASSAULTBIN" ]; then
     # Create dir if doesn't exists
         sudo mkdir "$AUDIOASSAULTBIN"
@@ -71,19 +73,25 @@ aa_install () {
     #v2
     sudo cp -r "$TEMPDIR/RVXX/RVXX v2/RVXX v2 Linux/RVXX" "$AUDIOASSAULTBIN"
     sudo cp -r "$TEMPDIR/RVXX/RVXX v2/RVXX v2 Linux/RVXX v2 Standalone" "$AUDIOASSAULTBIN/RVXX"
+    
     sudo cp -r "$TEMPDIR/Sigma/Sigma v2/Sigma v2 Linux/Sigma" "$AUDIOASSAULTBIN"
     sudo cp -r "$TEMPDIR/Sigma/Sigma v2/Sigma v2 Linux/Sigma v2 Standalone" "$AUDIOASSAULTBIN/Sigma"
 
     # v1
     sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality" "$AUDIOASSAULTBIN"
     sudo cp -r "$TEMPDIR/Duality/Linux 1.2.5/Duality Bass Studio" "$AUDIOASSAULTBIN/Duality"
+
     sudo cp -r "$TEMPDIR/RVXX/RVXX Installer 1.0.1/RVXX Linux /RVXX STANDALONE 101" "$AUDIOASSAULTBIN/RVXX"
     sudo cp -r "$TEMPDIR/Sigma/Sigma Standalone 102" "$AUDIOASSAULTBIN/Sigma"
+
+    sudo cp -r "$TEMPDIR/BlackSun/Blacksun Linux/Blacksun" "$AUDIOASSAULTBIN"
+    sudo cp -r "$TEMPDIR/BlackSun/Blacksun Linux/Blacksun Standalone" "$AUDIOASSAULTBIN/Blacksun"
 
     #chmod 777
     sudo chmod -R 777 "$AUDIOASSAULTBIN/Duality/"
     sudo chmod -R 777 "$AUDIOASSAULTBIN/RVXX/"
     sudo chmod -R 777 "$AUDIOASSAULTBIN/Sigma/"
+    sudo chmod -R 777 "$AUDIOASSAULTBIN/Blacksun/"
 
     # chmod
     #v2
@@ -93,6 +101,7 @@ aa_install () {
     sudo chmod +x "$AUDIOASSAULTBIN/Duality/Duality Bass Studio"
     sudo chmod +x "$AUDIOASSAULTBIN/RVXX/RVXX STANDALONE 101"
     sudo chmod +x "$AUDIOASSAULTBIN/Sigma/Sigma Standalone 102"
+    sudo chmod +x "$AUDIOASSAULTBIN/Blacksun/Blacksun Standalone"
 
     # usr/local/bin links
     sudo ln -s "$AUDIOASSAULTBIN/RVXX/RVXX v2 Standalone" "/usr/local/bin/rvxx_v2"
@@ -101,6 +110,7 @@ aa_install () {
     sudo ln -s "$AUDIOASSAULTBIN/Duality/Duality Bass Studio" "/usr/local/bin/duality-bass-studio_v1"
     sudo ln -s "$AUDIOASSAULTBIN/RVXX/RVXX STANDALONE 101" "/usr/local/bin/rvxx_v1"
     sudo ln -s "$AUDIOASSAULTBIN/Sigma/Sigma Standalone 102" "/usr/local/bin/sigma_v1"
+    sudo ln -s "$AUDIOASSAULTBIN/Blacksun/Blacksun Standalone" "/usr/local/bin/blacksun_v1"
 
     # copy .local files
     sudo cp -r usr/share /usr
@@ -118,16 +128,18 @@ aa_uninstall () {
     # @temp
     sudo rm -rf "$TEMPDIR"
     # desktop files
-    sudo rm -rf /usr/share/applications/sigma_v2.desktop
     sudo rm -rf /usr/share/applications/rvxx_v2.desktop
+    sudo rm -rf /usr/share/applications/sigma_v2.desktop
     #v1
     sudo rm -rf /usr/share/applications/duality-bass-studio_v1.desktop
     sudo rm -rf /usr/share/applications/rvxx_v1.desktop
     sudo rm -rf /usr/share/applications/sigma_v1.desktop
+    sudo rm -rf /usr/share/applications/blacksun_v1.desktop
     # icons
-    sudo rm -rf /usr/share/icons/sigma.png
-    sudo rm -rf /usr/share/icons/rvxx.png
     sudo rm -rf /usr/share/icons/duality-bass-studio.png
+    sudo rm -rf /usr/share/icons/rvxx.png
+    sudo rm -rf /usr/share/icons/sigma.png
+    sudo rm -rf /usr/share/icons/blacksun.png
     # vst
     sudo rm -rf "$AUDIOASSAULT"
     # bin
@@ -138,6 +150,7 @@ aa_uninstall () {
     sudo rm -rf /usr/local/bin/duality-bass-studio_v1
     sudo rm -rf /usr/local/bin/rvxx_v1
     sudo rm -rf /usr/local/bin/sigma_v1
+    sudo rm -rf /usr/local/bin/blacksun_v1
 
     echo "Done!!!"
 }
